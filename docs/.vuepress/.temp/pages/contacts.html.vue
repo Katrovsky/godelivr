ae720fc262b4993b0de2647c07c3dbed8e1882da
Katrovsky <katrovskykirill@gmail.com>
2024-07-15 22:18:53 +0400

    Added Contacts

---
+++ docs/.vuepress/.temp/pages/contacts.html.vue
@@ -0,0 +1,23 @@
+<template><div><h1 id="контакты" tabindex="-1"><a class="header-anchor" href="#контакты"><span>Контакты</span></a></h1>
+<h2 id="горячая-линия" tabindex="-1"><a class="header-anchor" href="#горячая-линия"><span>Горячая линия</span></a></h2>
+<p>Горячая линия для партнёров Сбермаркета -- <a href="+7 800 333-24-28">+7 800 333-24-28</a></p>
+<p>Горячая линия поддержки клиентов -- <a href="+7 800 222-11-00">+7 800 222-11-00</a></p>
+<div class="custom-container tip"><p class="custom-container-title">Подсказка</p>
+<p>Второй номер может пригодиться, но для трудовой деятельности он не нужен.</p>
+</div>
+<h2 id="саиты" tabindex="-1"><a class="header-anchor" href="#саиты"><span>Сайты</span></a></h2>
+<ul>
+<li><a href="https://partner.sbermarket-mobile.ru/" target="_blank" rel="noopener noreferrer">Личный кабинет партнёра</a> &gt; Только для самозанятых, устроившихся напрямую в Сбермаркет, без участия посредников</li>
+<li><a href="contact/Offer.pdf" title="ссылка на pdf-файл">ДОГОВОР ОФЕРТЫ</a></li>
+<li><a href="https://lk.mysbertips.ru/" target="_blank" rel="noopener noreferrer">СберЧаевые</a>{ align=right }</li>
+<li><a href="https://partnersbenefits.sbermarket.ru/" target="_blank" rel="noopener noreferrer">Программа привилегий для сборщиков и курьеров СберМаркета</a></li>
+</ul>
+<div class="custom-container warning"><p class="custom-container-title">Обратите внимание</p>
+<p>Некоторые страницы сайта &quot;Программа привилегий&quot; являются устаревшими:</p>
+<p><a href="https://partnersbenefits.sbermarket.ru/geekbrains" target="_blank" rel="noopener noreferrer">пример 1</a> — промокод двухлетней давности,</p>
+<p><a href="https://partnersbenefits.sbermarket.ru/minako" target="_blank" rel="noopener noreferrer">пример 2</a> — ссылка для получения промокода ведёт к неактуальному боту,</p>
+<p><a href="https://partner-sbermarket.tilda.ws/forward" target="_blank" rel="noopener noreferrer">пример 3</a> — то же, что в предыдущем пункте + указанный сайт изменил адрес и название. Возможно, домен использует другая фирма, на обращения по почте владельцы не отвечают.</p>
+</div>
+</div></template>
+
+
