3139b0a61190ae06caa3f277a2120e221f0240c7
Katrovsky <katrovskykirill@gmail.com>
2024-07-16 04:37:44 +0400

    .

--- docs/.vuepress/.temp/pages/contacts.html.js
+++ docs/.vuepress/.temp/pages/contacts.html.js
@@ -1,5 +1,5 @@
 import comp from "/home/key/Documents/SITE_BUILDING/katrovsky.github.io/docs/.vuepress/.temp/pages/contacts.html.vue"
-const data = JSON.parse("{\"path\":\"/contacts.html\",\"title\":\"Контакты\",\"lang\":\"ru-RU\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Горячая линия\",\"slug\":\"горячая-линия\",\"link\":\"#горячая-линия\",\"children\":[]},{\"level\":2,\"title\":\"Сайты\",\"slug\":\"саиты\",\"link\":\"#саиты\",\"children\":[]}],\"git\":{\"updatedTime\":1721070599000,\"contributors\":[{\"name\":\"Katrovsky\",\"email\":\"katrovskykirill@gmail.com\",\"commits\":5}]},\"filePathRelative\":\"contacts.md\"}")
+const data = JSON.parse("{\"path\":\"/contacts.html\",\"title\":\"Контакты\",\"lang\":\"ru-RU\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"Горячая линия\",\"slug\":\"горячая-линия\",\"link\":\"#горячая-линия\",\"children\":[]},{\"level\":2,\"title\":\"Сайты\",\"slug\":\"саиты\",\"link\":\"#саиты\",\"children\":[]}],\"git\":{\"updatedTime\":1721082350000,\"contributors\":[{\"name\":\"Katrovsky\",\"email\":\"katrovskykirill@gmail.com\",\"commits\":6}]},\"filePathRelative\":\"contacts.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/.temp/pages/shopper.html.js
+++ docs/.vuepress/.temp/pages/shopper.html.js
@@ -1,5 +1,5 @@
 import comp from "/home/key/Documents/SITE_BUILDING/katrovsky.github.io/docs/.vuepress/.temp/pages/shopper.html.vue"
-const data = JSON.parse("{\"path\":\"/shopper.html\",\"title\":\"Shopper\",\"lang\":\"ru-RU\",\"frontmatter\":{\"title\":\"Shopper\"},\"headers\":[],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"shopper.md\"}")
+const data = JSON.parse("{\"path\":\"/shopper.html\",\"title\":\"Shopper\",\"lang\":\"ru-RU\",\"frontmatter\":{\"title\":\"Shopper\"},\"headers\":[],\"git\":{\"updatedTime\":1721082350000,\"contributors\":[{\"name\":\"Katrovsky\",\"email\":\"katrovskykirill@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"shopper.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
