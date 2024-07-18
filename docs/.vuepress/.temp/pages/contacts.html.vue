1f71d647f4ecbbb994aa3c3f41045f60851b7b5b
Katrovsky <katrovskykirill@gmail.com>
2024-07-18 22:14:04 +0400

    next update

--- docs/.vuepress/.temp/pages/contacts.html.vue
+++ docs/.vuepress/.temp/pages/contacts.html.vue
@@ -1,13 +1,13 @@
 <template><div><h1 id="контакты" tabindex="-1"><a class="header-anchor" href="#контакты"><span>Контакты</span></a></h1>
 <h2 id="горячая-линия" tabindex="-1"><a class="header-anchor" href="#горячая-линия"><span>Горячая линия</span></a></h2>
-<p>Горячая линия для партнёров Сбермаркета &gt; <a href="tel:+78003332428">+7 800 333-24-28</a></p>
-<p>Горячая линия поддержки клиентов &gt; <a href="tel:+78002221100">+7 800 222-11-00</a></p>
+<p>Горячая линия для партнёров Купера &gt; <a href="tel:+78003332428">+7800333-24-28</a></p>
+<p>Горячая линия поддержки клиентов &gt; <a href="tel:+78002221100">+7800222-11-00</a></p>
 <div class="custom-container tip"><p class="custom-container-title">Подсказка</p>
 <p>Второй номер может пригодиться, но для трудовой деятельности он не нужен.</p>
 </div>
 <h2 id="саиты" tabindex="-1"><a class="header-anchor" href="#саиты"><span>Сайты</span></a></h2>
 <ul>
-<li><a href="https://partner.sbermarket-mobile.ru/" target="_blank" rel="noopener noreferrer">Личный кабинет партнёра</a> &gt; Только для самозанятых, устроившихся напрямую в Сбермаркет, без участия посредников</li>
+<li><a href="https://partner.sbermarket-mobile.ru/" target="_blank" rel="noopener noreferrer">Личный кабинет партнёра</a> &gt; Только для самозанятых, устроившихся напрямую в Купер, без участия посредников</li>
 <li>ДОГОВОР ОФЕРТЫ:
 <ul>
 <li><a href="/docs/Offer.pdf">Ссылка на pdf-файл Сбермаркета (старый договор)</a></li>
@@ -15,7 +15,7 @@
 </ul>
 </li>
 <li><a href="https://lk.mysbertips.ru/" target="_blank" rel="noopener noreferrer">СберЧаевые</a></li>
-<li><a href="https://partnersbenefits.sbermarket.ru/" target="_blank" rel="noopener noreferrer">Программа привилегий для сборщиков и курьеров СберМаркета</a></li>
+<li><a href="https://partnersbenefits.sbermarket.ru/" target="_blank" rel="noopener noreferrer">Программа привилегий для сборщиков и курьеров Купера</a></li>
 </ul>
 <div class="custom-container warning"><p class="custom-container-title">Обратите внимание</p>
 <p>Некоторые страницы сайта &quot;Программа привилегий&quot; являются устаревшими:</p>
